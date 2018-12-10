9dd6f5bb3831ed449602e083e0c2d6757aac7a1f
camilaodorizzi <camila.odorizzi@hotmail.com>
2018-12-10 21:24:49 -0200

    exame

---
+++ src/main/java/udesc/ceavi/dsw/ExameCamilaOdorizzi/ExameCamilaOdorizzi/ExameCamilaOdorizziApplication.java
@@ -0,0 +1,12 @@
+package udesc.ceavi.dsw.ExameCamilaOdorizzi.ExameCamilaOdorizzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameCamilaOdorizziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameCamilaOdorizziApplication.class, args);
+	}
+}
---
+++ src/test/java/udesc/ceavi/dsw/ExameCamilaOdorizzi/ExameCamilaOdorizzi/ExameCamilaOdorizziApplicationTests.java
@@ -0,0 +1,16 @@
+package udesc.ceavi.dsw.ExameCamilaOdorizzi.ExameCamilaOdorizzi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExameCamilaOdorizziApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
